42c3bd4e199ad43a0c5fbf3d31c1d5c8ee920699
José Luiz Marques Maciel <41921890+MacielMjose@users.noreply.github.com>
2019-03-18 21:59:05 -0300

    Add files via upload

---
+++ ElloBudda.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 5
+
+
+void hello();
+
+int main()
+{
+	hello();
+	
+   return 0;
+}
+
+void hello()
+{
+	printf("Elo my Budda\n");
+}
+
+
+
+
