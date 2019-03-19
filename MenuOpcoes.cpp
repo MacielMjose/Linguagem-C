7da991915ee3b06068902d5fe19165d7c535b790
José Luiz Marques Maciel <41921890+MacielMjose@users.noreply.github.com>
2019-03-18 22:30:19 -0300

    Add files via upload

---
+++ MenuOpcoes.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#define Tam 10
+
+
+void oi();
+void tudobem();
+void familia();
+void sair();
+
+int main()
+{
+	int continuar;
+
+	do{
+
+		printf("Escolha uma opcao:\n");
+		printf("1 - oi:\n");
+		printf("2 - Tudo bem?:\n");
+		printf("3 - Familia:\n");
+		printf("4 - Sair:\n");
+		printf("Opcao:");
+
+		scanf("%i",&continuar);
+		system("cls");
+
+        switch (continuar)
+			{
+						case 1:
+								oi();
+
+					break;
+
+						case 2:
+
+                             familia();
+
+					break;
+
+						case 3:
+
+						  	tudobem();
+
+                    break;
+
+						case 4:
+
+							sair();
+
+					break;
+
+				default:
+
+                    printf("Opcao invalida!\n");
+                    system("cls");
+
+                break;
+
+			}
+
+	  }while(continuar != 4);
+
+   return 0;
+
+}
+void oi()
+{
+
+	printf("RoBot_says(^.^): oi!\n");
+	system("pause");
+    system("cls");
+
+
+}
+void tudobem()
+{
+	printf("RoBot_says(^.^):tudo bem?\n");
+	system("pause");
+    system("cls");
+
+}
+void familia()
+{
+
+    printf("RoBot_says(^.^):como vai a familia?\n");
+    system("pause");
+    system("cls");
+
+}
+void sair()
+{
+	printf("RoBot_says(^.^):Adeus ;( \n");
+	system("pause");
+    system("cls");
+}
+
+
+
+
+
+
+
